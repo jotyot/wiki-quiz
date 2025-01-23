0ab21e6b395c98415e286adb971ad6030e55f099
jotyot <104177975+jotyot@users.noreply.github.com>
2025-01-23 02:47:51 -0800

    Update page.tsx

--- app/play/[id]/page.tsx
+++ app/play/[id]/page.tsx
@@ -1,10 +1,13 @@
 import { Room } from "@/app/room";
 import { Game } from "@/app/ui/game";
 
-export default function Page(props: { params: { id: string } }) {
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+
   return (
-    <Room id={props.params.id}>
-      {props.params.id}
+    <Room id={id}>
+      {id}
       <Game />
     </Room>
   );
